d52678fd6ed92688dfdef68aaeb633ede11bf6f0
Christian Rickert <rc.login@icloud.com>
2020-11-09 18:56:58 -0700

    data processing fixed

--- phenoptrReports_memory_fix.py
+++ phenoptrReports_memory_fix.py
@@ -2,7 +2,7 @@
 
 """
     Name:       phenoptrReports_memory_fix
-    Version:    1.0 (2020-10-28)
+    Version:    1.0 (2020-11-09)
     Author:     Christian Rickert
     Group:      Human Immune Monitoring Shared Resource (HIMSR)
                 University of Colorado, Anschutz Medical Campus
@@ -51,53 +51,42 @@ def println(string=""):
 def unmerge_data(in_path='/home/user/', out_path='/home/user'):
     """ Imports data from a text file and writes out all columns on a per-file basis
         using the first column's data for labeling of individual export files. """
-    slides = 0
     with open(in_path, 'r') as in_file:
-        base = os.path.basename(in_path)
-        channel = base.split(" ")[0]
+        channel = os.path.basename(in_path).split(" ")[0]
         if not channel:
             channel = "Unk"
         folder = out_path + os.path.sep + channel
         println("\tFOLDER: \"" + folder + "\"")
         if not os.path.exists(folder):
             os.mkdir(folder)
-        name = os.path.splitext(base)[0].replace("Merge_cell_seg_data ", "")
-        current_slide = ""
-        previous_slide = ""
-        file_data = []
+        name = os.path.splitext(os.path.basename(in_path))[0].replace("Merge_cell_seg_data ", "")
         println("\t\tSLIDES:")
 
         for in_index, in_line in enumerate(in_file):
-            if in_index == 0: # header
+            if in_index == 0:  # header
+                file_data = []
                 header = in_line
-            elif in_index == 1:
-                data = in_line.split("\t")
-                current_slide = data[1][0:-7]  # first slide name
-                println("\t\t\t\t\"" + current_slide + "\"")
-                previous_slide = current_slide
-            else: # data
-                data = in_line.split("\t")
-                current_slide = data[1][0:-7]  # new slide name
-                if current_slide != previous_slide:  # slide changed
+                file_data.append(header)
+                current_slide = ""
+                previous_slide = ""
+            else:  # data
+                current_slide = in_line.split("\t")[1][0:-7]
+                if current_slide != previous_slide and previous_slide:  # slide changed
                     with open(folder + os.path.sep +\
                               name + " - " + previous_slide + ".txt", 'a') as out_file:
                         for _out_index, out_line in enumerate(file_data):
                             out_file.write(out_line)
-
-                    println("\t\t\t\t\"" + current_slide + "\"")
+                    println("\t\t\t\t\"" + current_slide + "\"")  # prepare next slide
                     file_data = []
                     file_data.append(header)
-                    previous_slide = current_slide
-                    slides += 1
-                else:
-                    file_data.append(in_line)
+                previous_slide = current_slide
+                file_data.append(in_line)
 
-        # write last slide before opening a new file
+        # write last slide before opening a new input file
         with open(folder + os.path.sep + name + " - " + previous_slide + ".txt", 'a') as out_file:
             for _out_index, out_line in enumerate(file_data):
                 out_file.write(out_line)
 
-    return slides
 
 #  constants & variables
 
@@ -118,11 +107,9 @@ if not os.path.exists(EXPORT_FOLDER):
     os.mkdir(EXPORT_FOLDER)
 
 for file in get_files(IMPORT_FOLDER, FILE_TARGET):
-    SLIDE_COUNT = 0
     println("\tFILE: \"" + file + "\"")
-    SLIDE_COUNT += unmerge_data(in_path=file, out_path=EXPORT_FOLDER)
+    unmerge_data(in_path=file, out_path=EXPORT_FOLDER)
     FILE_COUNT += 1
-    println("\tSLIDES: " + str(SLIDE_COUNT))
 
 print("UNMERGED FILES: " + str(FILE_COUNT) + ".")
 println(os.linesep)
