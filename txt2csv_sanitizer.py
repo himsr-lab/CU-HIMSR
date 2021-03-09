cc091d99342759f681de62ab9038ac6a245b8c70
Christian Rickert <rc.login@icloud.com>
2021-03-08 17:51:08 -0700

    header update

--- txt2csv_sanitizer.py
+++ txt2csv_sanitizer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Copyright 2021 Regents of the University of Colorado
+Copyright 2021 The Regents of the University of Colorado
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
