46a59dee373314aae6ecb98aa0a1761603dbd8ee
Christian Rickert <rc.login@icloud.com>
2021-03-26 13:53:32 -0700

    Add files via upload

---
+++ install_phenoptr-Reports.R
@@ -0,0 +1,47 @@
+#
+# phenoptr(Reports) Installation for Windows
+# Christian Rickert, 2021-03-26
+#
+
+
+# install R 4.0.4
+# see: https://cran.r-project.org/bin/windows/base/
+
+# install Rtools 40
+# see: https://cran.r-project.org/bin/windows/Rtools/
+
+# install RStudio Desktop 1.4.1106
+# see: https://www.rstudio.com/products/rstudio/download/#download
+
+
+#
+# WARNING: Do NOT install or update (from source) packages!
+#
+
+# install phenoptr
+# see: https://akoyabio.github.io/phenoptr/
+
+# install devtools package
+if ( !require("devtools") )
+  install.packages("devtools")
+
+# install phenoptr plugin from GitHub
+# hint: do not update packages to newer (source) versions
+devtools::install_github("akoyabio/phenoptr@master")
+
+# install tiff package for phenoptr
+# see: https://github.com/akoyabio/tiff/tree/master#installation
+devtools::install_github("akoyabio/tiff@master")
+#devtools::install_github("akoyabio/phenoptr@9000")
+
+# install rtree package for phenoptr
+# see: https://github.com/akoyabio/rtree/tree/master#installation
+devtools::install_github("akoyabio/rtree@BH_1.75")
+
+
+# install phenoptrReports plugin from GitHub
+# see: https://akoyabio.github.io/phenoptrReports/
+
+# install phenoptrReports plugin from GitHub
+devtools::install_github("akoyabio/phenoptrReports@master")
+#devtools::install_github("akoyabio/phenoptrReports@9000")
