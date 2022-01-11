84f726b6fed879d9929395e4f6846b793829a5a7
Christian Rickert <rc.login@icloud.com>
2022-01-10 17:27:18 -0700

    updated inForm version dependency

--- install_phenoptr-Reports.R
+++ install_phenoptr-Reports.R
@@ -1,20 +1,21 @@
 #
 # phenoptr(Reports) Installation for Windows
-# Christian Rickert, 2021-08-24
+# Christian Rickert, 2022-10-10
 #
 
-# install R 4.1.0
+# install R 4.1.1 or later
 # see: https://cran.r-project.org/bin/windows/base/
 
-# install Rtools 40
+# install Rtools 40 or later
 # see: https://cran.r-project.org/bin/windows/Rtools/
 
-# install RStudio Desktop 1.4.1106
+# install RStudio Desktop 1.4.1717 or later
 # see: https://www.rstudio.com/products/rstudio/download/#download
 
 #
 # WARNING: Do NOT install or update (from source) packages!
-#
+#          Select "3: None" (type "3" and hit Enter/Return)
+#          when prompted to update to more recent packages.
 
 # install devtools package
 install.packages("devtools")
@@ -23,10 +24,11 @@ install.packages("devtools")
 # see: https://akoyabio.github.io/phenoptr/
 
 # install phenoptr plugin from GitHub
-# hint: new stable versions are located in the @master branch,
-# and new development versions are located in the @9000 branch
-devtools::install_github("akoyabio/phenoptr@master")
-#devtools::install_github("akoyabio/phenoptr@9000")
+# hint: versions compatible with inForm version 2.5 (or earlier)
+# are located in the @head branch, while versions compatible
+# with inForm 2.6 (or later) are located in the @9000 branch
+#devtools::install_github("akoyabio/phenoptr")
+devtools::install_github("akoyabio/phenoptr@9000")
 
 # install tiff package for phenoptr
 # see: https://github.com/s-u/tiff
@@ -40,7 +42,7 @@ devtools::install_github("akoyabio/rtree@master")
 # see: https://akoyabio.github.io/phenoptrReports/
 
 # install phenoptrReports plugin from GitHub
-# hint: new stable versions are located in the @master branch,
+# hint: new stable versions are located in the @head branch,
 # and new development versions are located in the @9000 branch
-devtools::install_github("akoyabio/phenoptrReports@master")
+devtools::install_github("akoyabio/phenoptrReports@head")
 #devtools::install_github("akoyabio/phenoptrReports@9000")
