5562140e2b8f18e7fb422a97ee217a075ac4d7ce
Christian Rickert <rc.login@icloud.com>
2024-03-20 20:15:21 -0600

    Update install_phenoptr-Reports.R

--- install_phenoptr-Reports.R
+++ install_phenoptr-Reports.R
@@ -1,15 +1,15 @@
 #
 # phenoptr(Reports) Installation for Windows
-# Christian Rickert, 2023-11-13
+# Christian Rickert, 2024-03-20
 #
 
-# install R 4.1.0 or later
+# install R 4.3.3 or later
 # see: (Windows)  https://cran.r-project.org/bin/windows/base/old/
 
-# install Rtools 4.0 or later
+# install Rtools 4.3 or later
 # see: (Windows)  https://cran.r-project.org/bin/windows/Rtools/
 
-# install RStudio Desktop 2022.07.0 or later
+# install RStudio Desktop 2022.12.1 or later
 # see: https://posit.co/download/rstudio-desktop/
 
 #
@@ -25,14 +25,6 @@ install.packages("devtools")
 
 # install phenoptr plugin from GitHub
 devtools::install_github("christianrickert/phenoptr")
-
-# install tiff package for phenoptr
-# see: https://github.com/s-u/tiff
-install.packages("tiff")
-
-# install rtree package for phenoptr
-# see: https://github.com/akoyabio/rtree/tree/master#installation
-devtools::install_github("akoyabio/rtree@master")
 
 # phenoptrReports
 # see: https://akoyabio.github.io/phenoptrReports/
